49d378bb1a0b3997e0f2abd4fbe52928a00d14fb
Mate <mate.miticzky@gmail.com>
2024-05-10 16:46:48 +0200

    Teszt hozzaadasa

---
+++ src/test/java/eu/runnrrace/nnkwo5/BeadandoApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.runnrrace.nnkwo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeadandoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
