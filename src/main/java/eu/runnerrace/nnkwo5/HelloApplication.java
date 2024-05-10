af405e21b0c89a05bd5c26ec82128c0022fd3f50
Mate <mate.miticzky@gmail.com>
2024-05-10 15:40:26 +0200

    First commit, default project structure

---
+++ src/main/java/eu/runnerrace/nnkwo5/HelloApplication.java
@@ -0,0 +1,9 @@
+package eu.runnerrace.nnkwo5;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
