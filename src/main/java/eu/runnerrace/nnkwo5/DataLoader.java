8bbf48f4d5d9ea34abc536a390bfd34d7ceee96c
mateusz200156 <mate.miticzky@gmail.com>
2024-05-10 17:05:13 +0200

    Update DataLoader.java

--- src/main/java/eu/runnerrace/nnkwo5/DataLoader.java
+++ src/main/java/eu/runnerrace/nnkwo5/DataLoader.java
@@ -24,36 +24,36 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         //Versenyek
         VersenyEntry v1 = new VersenyEntry();
-        v1.setNev("Alfa verseny");
+        v1.setNev("Elso verseny");
         v1.setTavolsag(10);
         versenyRepository.save(v1);
 
         VersenyEntry v2 = new VersenyEntry();
-        v2.setNev("Beta verseny");
+        v2.setNev("Masodik verseny");
         v2.setTavolsag(2);
         versenyRepository.save(v2);
 
         //Versenyzők
         VersenyzoEntry vz1 = new VersenyzoEntry();
-        vz1.setNev("David");
-        vz1.setKor(22);
+        vz1.setNev("Béla");
+        vz1.setKor(27);
         vz1.setIsFerfi(true);
         versenyzoRepository.save(vz1);
 
         VersenyzoEntry vz2 = new VersenyzoEntry();
-        vz2.setNev("Izabella");
-        vz2.setKor(20);
+        vz2.setNev("Maria");
+        vz2.setKor(18);
         vz2.setIsFerfi(false);
         versenyzoRepository.save(vz2);
 
         VersenyzoEntry vz3 = new VersenyzoEntry();
-        vz3.setNev("Peti");
-        vz3.setKor(35);
+        vz3.setNev("Jozsef");
+        vz3.setKor(42);
         vz3.setIsFerfi(true);
         versenyzoRepository.save(vz3);
 
         VersenyzoEntry vz4 = new VersenyzoEntry();
-        vz4.setNev("Máté");
+        vz4.setNev("Huan");
         vz4.setKor(28);
         vz4.setIsFerfi(true);
         versenyzoRepository.save(vz4);
